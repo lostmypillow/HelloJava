79775a37bf7c8ab269da05109980b3082305616b
Johnny <47119111+lostmypillow@users.noreply.github.com>
2024-05-31 11:58:24 +0800

    20240531

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -2,12 +2,23 @@ import java.util.Scanner;
 
 public class HelloJava {
     public static void main(String[] args) {
-System.out.println("Once upon a time");
-System.out.println("there was");
-System.out.println("a program");
+        double a = 3.14;
+        double b = 22.0;
+        double c = a * b * b;
+
+        System.out.println(c);
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a program");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Write a message: ");
         String message = scanner.nextLine();
         System.out.println("u wrote " + message);
+        String next_message = scanner.nextLine();
+        System.out.println("well well " + next_message);
+        int value = 10;
+        System.out.println(value);
+        value = 4;
+        System.out.println(value);
     }
     }
